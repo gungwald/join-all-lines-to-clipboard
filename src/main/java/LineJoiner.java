e596bead0cc6ef8f8b809b3f7502250742549605
Bill Chatfield <bill_chatfield@yahoo.com>
2020-06-14 14:04:24 -0400

    Join lines not just to clipboard

---
+++ src/main/java/LineJoiner.java
@@ -0,0 +1,69 @@
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class LineJoiner {
+
+	/**
+	 * Executions start here
+	 * 
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+		    LineJoiner j = new LineJoiner();
+            j.joinLinesFromInputSources(args);
+		} 
+        catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+    public void joinLinesFromInputSources(String[] args) throws IOException {
+        if (args.length > 0) {
+            // Read lines from files named on the command line.
+            for (int i = 0; i < args.length; i++) {
+                joinLines(new File(args[i]));
+            }
+        } 
+        else {
+            // Read lines from standard input.
+            joinLines(System.in);
+        }
+    }
+
+    public void joinLines(File f) throws IOException {
+        joinLines(new BufferedReader(new FileReader(f)));
+    }
+
+    public void joinLines(InputStream in) throws IOException {
+        joinLines(new BufferedReader(new InputStreamReader(in)));
+    }
+
+	public void joinLines(BufferedReader in) throws IOException {
+		try {
+			String line;
+			while ((line = in.readLine()) != null) {
+                System.out.print(line.trim());
+                System.out.print(' ');
+			}
+		} 
+        finally {
+			in.close();
+		}
+	}
+
+	public void copyToClipboard(String text) {
+		StringSelection selection = new StringSelection(text);
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(selection, selection);
+		System.out.println(text);
+	}
+    
+}
